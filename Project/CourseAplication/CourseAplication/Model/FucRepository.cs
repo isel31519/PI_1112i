ae125d2a621ff21c88b3e3e3116c8de00560b0c0
isel31519 <31519@alunos.isel.pt>
2011-10-13 15:06:22 +0100

    organizacao

--- Project/CourseAplication/CourseAplication/Model/FucRepository.cs
+++ Project/CourseAplication/CourseAplication/Model/FucRepository.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 
-namespace PI.CourseAplication.Model
+namespace CourseAplication.Model
 {
     class FucRepository
     {
