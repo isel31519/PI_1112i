72c414c02e5f6799743c85b1e8446f2a6a8f0d21
isel31519 <31519@alunos.isel.pt>
2011-10-13 16:47:28 +0100

    project prop

--- Project/CourseAplication/CourseAplication/Model/FucProposal.cs
+++ Project/CourseAplication/CourseAplication/Model/FucProposal.cs
@@ -2,14 +2,14 @@
 
 namespace CourseAplication.Model
 {
-    class FucProposal:Fuc
+    class FucProposal//:Fuc
     {
-        private int _id;
+        /*private int _id;
         private int _userId;
         public FucProposal(string name, string acr, bool req, List<ushort> sems, List<Fuc> reqmts, double ects, List<string> desc,int user) 
             : base(name, acr, req, sems, reqmts, ects, desc)
         {
             _userId = user;
-        }
+        }*/
     }
 }
