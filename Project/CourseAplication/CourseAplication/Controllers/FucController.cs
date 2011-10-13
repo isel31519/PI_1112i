6877a28e876fe32186a6c01edf51000b3fc54329
isel31519 <31519@alunos.isel.pt>
2011-10-13 16:19:57 +0100

    primeiras classes teste

--- Project/CourseAplication/CourseAplication/Controllers/FucController.cs
+++ Project/CourseAplication/CourseAplication/Controllers/FucController.cs
@@ -1,11 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using CourseAplication.Model;
+using PI.WebGarten;
+using PI.WebGarten.MethodBasedCommands;
 
 namespace CourseAplication.Controllers
 {
     class FucController
     {
+         private readonly FucRepository _repo;
+         public FucController()
+        {
+            _repo = FucRepositoryLocator.Get();
+        }
+
+        [HttpCmd(HttpMethod.Get, "/fuc/{id}")]
+        public HttpResponse Get(string id)
+        {
+            var td = _repo.GetByAcr(id);
+            return td == null ? new HttpResponse(HttpStatusCode.NotFound) : new HttpResponse(200, new FucView(td));
+        }
     }
+
 }
--- Project/CourseAplication/CourseAplication/Program.cs
+++ Project/CourseAplication/CourseAplication/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using CourseAplication.Controllers;
 using CourseAplication.Model;
 using PI.WebGarten;
 using PI.WebGarten.MethodBasedCommands;
@@ -9,15 +10,15 @@ namespace CourseAplication
     {
         static void Main(string[] args)
         {
-           
-            var repo = new FucRepository();
+
+            var repo =  FucRepositoryLocator.Get();
             repo.Add(new Fuc("Programaçao Orientada a Objectos", "POO", true, 2, "Pg", 6.0, "Tipo cenas"));
             //repo.Add(new Fuc;
 
             var host = new HttpListenerBasedHost("http://localhost:8080/");
             host.Add(DefaultMethodBasedCommandFactory.GetCommandsFor(
-                typeof(FucContrller),
-                typeof(ToDoController)));
+                typeof(FucController)
+               ));
             host.OpenAndWaitForever();
         }
     }
