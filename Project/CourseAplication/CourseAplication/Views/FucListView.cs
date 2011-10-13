41ceb9e764e4b900c89a816c141b5bef66307246
a31080 <31080@alunos.isel.pt>
2011-10-13 17:06:42 +0100

    created FucListView; edited RootView

---
+++ Project/CourseAplication/CourseAplication/Views/FucListView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PI.WebGarten;
+using PI.WebGarten.HttpContent.Html;
+
+namespace CourseAplication.Views
+{
+    class FucListView : HtmlDoc
+    {
+        public FucListView(string t, params IWritable[] content)
+            : base("FUC index",
+                H1(Text("Lista de FUC")),
+                Ul(
+                    t.Select(td => Li(A(ResolveUri.For(td), td.Description))).ToArray()
+                   )
+                   ) { }
+    }
+}
