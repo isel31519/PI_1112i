cb9a200bac1845f849b28dea67d18c04bbaaddcc
a31080 <31080@alunos.isel.pt>
2011-10-13 15:18:39 +0100

    created root view

---
+++ Project/CourseAplication/CourseAplication/Views/RootView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PI.WebGarten;
+using PI.WebGarten.HttpContent.Html;
+
+namespace CourseAplication.Views
+{
+    class RootView : HtmlDoc
+    {
+        public RootView(string t, params IWritable[] content) : base(t, content)
+        {
+            t = "Root fuc";
+        }
+    }
+}
