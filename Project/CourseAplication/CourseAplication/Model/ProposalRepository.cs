a9cabea6c7c9a8a02e6d0437801842eb53411d2c
isel31519 <31519@alunos.isel.pt>
2011-10-17 16:54:40 +0100

    Models created; Fuc and FucProposal edited

--- Project/CourseAplication/CourseAplication/Model/ProposalRepository.cs
+++ Project/CourseAplication/CourseAplication/Model/ProposalRepository.cs
@@ -7,5 +7,25 @@ namespace CourseAplication.Model
 {
     class ProposalRepository
     {
+        private readonly IDictionary<int, FucProposal> _repo = new Dictionary<int, FucProposal>();
+        private int _cid = 0;
+
+        public IEnumerable<FucProposal> GetAll()
+        {
+            return _repo.Values;
+        }
+
+        public FucProposal GetById(int id)
+        {
+            FucProposal td = null;
+            _repo.TryGetValue(id, out td);
+            return td;
+        }
+
+        public void Add(FucProposal td)
+        {
+            td.Id = _cid;
+            _repo.Add(_cid++, td);
+        }
     }
 }
