c97c549d787f5a3b97a90d908ec79f359d6ebb76
a31080 <31080@alunos.isel.pt>
2011-10-21 14:43:41 +0100

    created PrincipalParameterBinder

---
+++ WebGarten/PI.WebGarten/MethodBasedCommands/PrincipalParameterBinder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+
+namespace PI.WebGarten.MethodBasedCommands
+{
+    class PrincipalParameterBinder : IParameterBinder
+    {
+        public Func<RequestInfo, object> TryGetBinder(ParameterInfo pi, HttpCmdAttribute attr)
+        {
+            if(pi.ParameterType == typeof(HttpListenerRequest))
+            {
+                return ri => ri.User;
+            }
+            return null;
+        }
+    }
+}
