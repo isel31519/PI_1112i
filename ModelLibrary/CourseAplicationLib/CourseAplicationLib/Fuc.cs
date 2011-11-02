04c3f0e62c3a62520c024cac6f945dc78d0e1226
31503-isel <nelsonalmendra@gmail.com>
2011-11-02 16:03:36 +0000

    Changed Fuc.cs and FucProposal.cs. In Fuc.cs all the lists are now strings. FucProposal receives now only the user.

--- ModelLibrary/CourseAplicationLib/CourseAplicationLib/Fuc.cs
+++ ModelLibrary/CourseAplicationLib/CourseAplicationLib/Fuc.cs
@@ -4,87 +4,27 @@ namespace CourseAplicationLib
 {
     public class Fuc
     {
-        private string _name;
-        private string _acr;
-        private readonly bool _required;
-        private List<ushort> _semester;
-        private List<string> _prerequisites;
-        private double _ects;
-        private Dictionary<string,string> _description;
+        public Fuc() {}
 
-
-        public Fuc(string name, string acr, bool req, double ects)
+        public Fuc(string name, string acr, bool req, string sem, string prereq, double ects)
         {
-            _name = name;
-            _acr = acr;
-            _required = req;
-            _semester = new List<ushort>();
-            _prerequisites = new List<string>();
-            _ects = ects;
-            _description = new Dictionary<string,string>();
+            Name = name;
+            Acr = acr;
+            IsRequired = req;
+            Semesters = sem;
+            Prerequisites = prereq;
+            Ects = ects;
         }
 
-        private string Get<T>(List<T> a)
-        {
-            string s = "";
-
-            foreach (T val in a)
-                s += val + " ";
-
-            return s;
-        }
-
-        public string GetSemesters()
-        {
-            return Get<ushort>(_semester);
-        }
-
-        public ushort Semester
-        {
-             set{ _semester.Add(value); }
-        }
-
-        public string GetPrerequisites()
-        {
-            return Get<string>(_prerequisites);
-        }
-
-        public string Prerequisites
-        {
-            set { _prerequisites.Add(value); }
-        }
-        
-        public void AddDescription(string title,string desc)
-        {
-            if(desc!=null)
-            _description.Add(title,desc); 
-        }
-        public string GetDescription(string title)
-        {
-            string desc;
-             _description.TryGetValue(title,out desc);
-            return desc;
-        }
-
-        public string Name
-        {
-            get{return _name;}
-            set{_name = value;}
-        }
-        public string Acr
-        {
-            get { return _acr; }
-            set { _acr = value; }
-        }
-        public bool IsRequired
-        {
-            get { return _required; }
-
-        }
-        public double Ects
-        {
-            get { return _ects; }
-            set { _ects = value; }
-        }
+        public string Name { get; set; }
+        public string Acr { get; set; }
+        public bool IsRequired { get; set; }
+        public string Semesters { get; set; }
+        public string Prerequisites { get; set; }
+        public double Ects { get; set; }
+        public string Objectives { get; set; }
+        public string Results { get; set; }
+        public string Evaluation { get; set; }
+        public string Program { get; set; }
     }
 }
--- ModelLibrary/CourseAplicationLib/CourseAplicationLib/FucProposal.cs
+++ ModelLibrary/CourseAplicationLib/CourseAplicationLib/FucProposal.cs
@@ -2,25 +2,12 @@
 {
     public class FucProposal:Fuc
     {
-        private int _id;
-        private string _userId;
-        public FucProposal(string name, string acr, bool req, double ects, string user) 
-            : base(name, acr, req,ects)
+        public FucProposal(string user)
         {
-            _userId = user;
+            User = user;
         }
 
-        public int Id
-        {
-            get { return _id; }
-            set { _id = value; }
-        }
-
-        public string User
-        {
-            get { return _userId; }
-            set { _userId = value; }
-        }
-
+        public int Id { get; set; }
+        public string User { get; set; }
     }
 }
