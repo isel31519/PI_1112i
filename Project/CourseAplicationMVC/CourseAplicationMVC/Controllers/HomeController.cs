0520f6348b4c61428166e8f5f7b8d00fac92203b
31503-isel <nelsonalmendra@gmail.com>
2011-11-03 19:42:06 +0000

    Created Login view and IndexUser view. Changed Index view for Home.

--- Project/CourseAplicationMVC/CourseAplicationMVC/Controllers/HomeController.cs
+++ Project/CourseAplicationMVC/CourseAplicationMVC/Controllers/HomeController.cs
@@ -5,21 +5,25 @@ using System.Web;
 using System.Web.Mvc;
 using CourseAplicationLib;
 
-
 namespace CourseAplicationMVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UserRepository _repo = RepositoryLocator.GetUserRep();
+        
         // GET: /Home/
         public ActionResult Index()
         {
-          
             return View();
         }
 
         public ActionResult Login()
         {
+            return View();
+        }
 
+        public ActionResult IndexUser(string user)
+        {
             return View();
         }
     }
