25373a0b94bf1fd331c2eb27556f9a0a20c852c1
a31080 <31080@alunos.isel.pt>
2011-11-03 18:13:20 +0000

    Created edit, detail and corrected create views from NewFucProposal. Also added the needed methods to the controller, of course.

--- Project/CourseAplicationMVC/CourseAplicationMVC/Controllers/NewFucProposalController.cs
+++ Project/CourseAplicationMVC/CourseAplicationMVC/Controllers/NewFucProposalController.cs
@@ -16,13 +16,34 @@ namespace CourseAplicationMVC.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            return View(_repo.GetAll());
         }
 
         public ActionResult Create()
         {
-            return View(_repo.GetAll());
+            return View();
         }
 
+        public ActionResult Detail(int id)
+        {
+            return View(_repo.GetById(id));
+        }
+
+        [HttpPost]
+        public ActionResult Create(FucProposal fp)
+        {
+            /*if (!ModelState.IsValid)
+              return View(_repo.GetById(f.Id));*/
+
+            _repo.Add(fp);
+            //bruta!!
+            return Redirect(string.Format("/{0}/{1}/{2}", "NewFucProposal", "Detail", fp.Id));
+        }
+
+        public ActionResult Edit(int id)
+        {
+            //notfound
+            return View(_repo.GetById(id));
+        }
     }
 }
