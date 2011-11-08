5457fe4062e2cafb2900aa07b36f873896f6b728
isel31519 <31519@alunos.isel.pt>
2011-11-08 22:45:22 +0000

    edited login action, first step to authenticate a user, created GenericPrincial

--- Project/CourseAplicationMVC/CourseAplicationMVC/Controllers/HomeController.cs
+++ Project/CourseAplicationMVC/CourseAplicationMVC/Controllers/HomeController.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Runtime.Remoting.Contexts;
+using System.Security.Principal;
 using System.Web.Mvc;
+using System.Web.Security;
 using CourseAplicationLib;
 
 namespace CourseAplicationMVC.Controllers
@@ -22,9 +21,53 @@ namespace CourseAplicationMVC.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Login(User u)
+        {
+            if (ModelState.IsValid)
+            {
+                User user;
+                if (!(user = _repo.GetById(u.Name)).Match(u.Pass)) return View();
+                {
+                    var principal = new GenericPrincipal(new GenericIdentity(u.Name), new string[] { user.Role });
+                    HttpContext.User = principal;
+
+                    FormsAuthentication.SetAuthCookie(u.Name, false);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Incorrect name or password.");
+            }
+
+            return View();
+
+            /*if (!ModelState.IsValid)
+            {
+                
+            }
+            User user;
+            if (!(user=_repo.GetById(u.Name)).Match(u.Pass)) return View();
+            var principal = new GenericPrincipal( new GenericIdentity(u.Name),new string[]{user.Role} );
+
+            FormsAuthentication.SetAuthCookie(user.Name, false);
+            /* FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(1, user.Email,
+            DateTime.Now, DateTime.Now.AddMinutes(FormsAuthentication.Timeout.TotalMinutes),
+            createPersistentCookie, userData);
+            string hashedTicket = FormsAuthentication.Encrypt(ticket);
+            HttpCookie cookie = new HttpCookie(FormsAuthentication.FormsCookieName, hashedTicket);
+            HttpContext.Current.Response.Cookies.Add(cookie);*/
+
+        }
+
+
         public ActionResult IndexUser(string user)
         {
-            return View();
+            if (HttpContext.User.Identity.IsAuthenticated)
+                return View();
+
+             return RedirectToAction("Login", "Home");
         }
     }
 }
