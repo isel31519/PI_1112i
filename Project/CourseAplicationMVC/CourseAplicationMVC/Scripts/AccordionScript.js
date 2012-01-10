c36120e7f521b3b6e8cff039de2ff6913da44b1d
a31080 <31080@alunos.isel.pt>
2012-01-10 20:41:04 +0000

    minor graphical changes to NewFucProposal view

---
+++ Project/CourseAplicationMVC/CourseAplicationMVC/Scripts/AccordionScript.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    $('a.detailer').click(function (e) {
+        e.preventDefault();
+        $('#showDetail').slideDown();
+        var http = new XMLHttpRequest();
+        console.log($(this).attr("href"));
+        http.open("GET", $(this).attr("href") + "?partial=true", false);
+        http.onreadystatechange = window.useHttpResponse;
+        http.send(null);
+
+        if (http.readyState == 4 && http.status == 200) {
+            var textout = http.responseText;
+            console.log(textout);
+            $('#showDetail').html(textout);
+        }
+    });
+});
