ed8dd7f58124f26348d7ae23748e52aa4e54ce18
a31080 <31080@alunos.isel.pt>
2012-01-16 17:09:03 +0000

    Created and implemented Search controller and respective view

---
+++ Project/CourseAplicationMVC/CourseAplicationMVC/Controllers/SearchController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CourseAplicationLib;
+
+namespace CourseAplicationMVC.Controllers
+{
+    public class SearchController : Controller
+    {
+        //
+        // GET: /Search/
+        private readonly FucRepository _repo = RepositoryLocator.GetFucRep();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(String searchQuery)
+        {
+            var fucList = _repo.GetAll();
+            var fucListToReturn = new List<Fuc>();
+
+            foreach (var fuc in fucList)
+            {
+                if(fuc.Name.ToLower().Contains(searchQuery))
+                    fucListToReturn.Add(fuc);
+            }
+
+            return View(fucListToReturn);
+        }
+
+    }
+}
